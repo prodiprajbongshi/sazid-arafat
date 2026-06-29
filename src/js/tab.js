90ff18f35c90f39ecae37c8052b3d38527815d40
prodiprajbongshi <devprodipraj@gmail.com>
2026-06-29 16:25:05 +0600

    change works page design add tab for work cards

---
+++ src/js/tab.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+
+    tab.addEventListener("click", () => {
+
+        tabs.forEach(btn => btn.classList.remove("active"));
+        contents.forEach(content => content.classList.remove("active"));
+
+        tab.classList.add("active");
+
+        document
+            .getElementById(tab.dataset.tab)
+            .classList.add("active");
+
+    });
+
+});
